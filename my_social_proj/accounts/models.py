f181929e3b0411459d1837b22b3b406c659bfcb2
gadekd <danielwgadek@gmail.com>
2022-11-08 15:04:37 +0100

    first commit; first app

---
+++ my_social_proj/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib import auth
+
+
+class User(auth.models.User, auth.models.PermissionMixin):
+
+    def __str__(self):
+        """Although we didn't assing value for username, it will still work, because it comes from auth.models.User"""
+        return "@{}".format(self.username)
