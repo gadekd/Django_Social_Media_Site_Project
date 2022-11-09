bd3455ccf0e79da675548fcba8c907d43a61d163
gadekd <danielwgadek@gmail.com>
2022-11-09 21:07:11 +0100

    urls, views and models for posts

--- my_social_proj/groups/views.py
+++ my_social_proj/groups/views.py
@@ -1,11 +1,18 @@
+# GROUPS VIEWS
+
 from django.shortcuts import render
 
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from django.urls import reverse
 from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.db import IntegityError
 
 from gropus.models import Group, GroupMember
+
+from . import models
 
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
@@ -19,3 +26,41 @@ class SingleGroup(generic.DeleteView):
 
 class ListGroups(generic.ListView):
     models = Group
+    
+    
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+    
+    def get_redirect_url(self, *args, **kwargs):
+        """Get redirected to the page with detail view without particular group"""
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+    
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+        
+        try:
+            # Try to create a group member with the credentials the same as current user
+            GroupMember.objects.create(user=self.request.user, group=group)
+            
+        except IntegityError:
+            messages.warning(self.request, 'Warning! Already a member!')
+        else:
+            messages.success(self.request, 'You are now a member!')
+            
+            
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+    
+    def get_redirect_url(self, *args, **kwargs):
+        """Get redirected to the page with detail view without particular group"""
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+    
+    def get(self, request, *args, **kwargs):
+        
+        try:
+            membership = models.GroupMember.objects.filter(user=self.request.user, group__slug=self.kwargs.get('slug'))
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry, you are not a member of this group!')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You have successfully left the group!')
+            
+        return super().get(request, *args, **kwargs)     
--- my_social_proj/posts/models.py
+++ my_social_proj/posts/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 import misaka
 
-from group.models import Group
+from groups.models import Group
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
